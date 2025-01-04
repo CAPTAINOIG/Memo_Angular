b4a5721290c8f02eda9be1e235a2b41da2bbee08
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2025-01-04 19:49:34 +0100

    commit

---
+++ src/app/competitor/competitor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competitor',
+  standalone: true,
+  imports: [],
+  templateUrl: './competitor.component.html',
+  styleUrl: './competitor.component.css'
+})
+export class CompetitorComponent {
+
+}
--- src/app/folder/folder.component.ts
+++ src/app/folder/folder.component.ts
@@ -3,11 +3,19 @@ import { NavbarComponent } from "../navbar/navbar.component";
 import { NavigatingComponent } from "../navigating/navigating.component";
 import { LandingpageusersComponent } from "../landingpageusers/landingpageusers.component";
 import { AddressblockchainComponent } from "../addressblockchain/addressblockchain.component";
+import { CompetitorComponent } from "../competitor/competitor.component";
+import { PlatformComponent } from "../platform/platform.component";
+import { TokenComponent } from "../token/token.component";
+import { BonusComponent } from "../bonus/bonus.component";
+import { TeamComponent } from "../team/team.component";
+import { DistributionComponent } from "../distribution/distribution.component";
+import { FooterComponent } from "../footer/footer.component";
+import { PartnerComponent } from "../partner/partner.component";
 
 @Component({
   selector: 'app-folder',
   standalone: true,
-  imports: [NavbarComponent, NavigatingComponent, LandingpageusersComponent, AddressblockchainComponent],
+  imports: [NavbarComponent, NavigatingComponent, LandingpageusersComponent, AddressblockchainComponent, CompetitorComponent, PlatformComponent, TokenComponent, BonusComponent, TeamComponent, DistributionComponent, FooterComponent, PartnerComponent],
   templateUrl: './folder.component.html',
   styleUrl: './folder.component.css'
 })
