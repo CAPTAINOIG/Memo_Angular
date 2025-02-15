0646a3c5a640a676969feaad29e01823758feccd
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2025-02-15 07:42:39 +0100

    changed to deployed url

--- src/app/service/HttpRequest/http-request.service.ts
+++ src/app/service/HttpRequest/http-request.service.ts
@@ -6,8 +6,8 @@ import { LocalstorageService } from '../LocalstorageService/localstorage.service
   providedIn: 'root'
 })
 export class HttpRequestService {
-  private baseUrl = 'https://c83f-2a09-bac5-4dd0-1c82-00-2d7-76.ngrok-free.app/api';
-  // private baseUrl = 'https://lendnode.creditclan.com/memo/api';
+  // private baseUrl = 'https://c83f-2a09-bac5-4dd0-1c82-00-2d7-76.ngrok-free.app/api';
+  private baseUrl = 'https://lendnode.creditclan.com/memo/api';
   private geoApiKey = '39f835849b07490f9ada6d2e31447933';
   
   constructor(
